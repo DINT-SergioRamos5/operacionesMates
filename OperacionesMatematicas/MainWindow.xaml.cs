96a6df155dc22e50a81b84473a1b2e973fc3d4d5
Sergio Ramos Santonja <sergioramos55@outlook.es>
2019-09-25 13:46:03 +0200

    Cambio en el proyecto

--- OperacionesMatematicas/MainWindow.xaml.cs
+++ OperacionesMatematicas/MainWindow.xaml.cs
@@ -23,11 +23,80 @@ namespace OperacionesMatematicas
         public MainWindow()
         {
             InitializeComponent();
+            Operando1TextBox.Text = "0";
+            Operando2TextBox.Text = "0";
+            ResultadoTextBox.Text = "0";
+            SumaCheckBox.IsChecked = true;
         }
 
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        private void Suma()
         {
-            SumaCheckBox.IsChecked;
+            if (Operando1TextBox.Text != "" && Operando2TextBox.Text != "")
+                ResultadoTextBox.Text = (double.Parse(Operando1TextBox.Text) + double.Parse(Operando2TextBox.Text)).ToString();
+        }
+
+        private void Resta()
+        {
+            if (Operando1TextBox.Text != "" && Operando2TextBox.Text != "")
+                ResultadoTextBox.Text = (double.Parse(Operando1TextBox.Text) - double.Parse(Operando2TextBox.Text)).ToString();
+        }
+
+        private void Multiplicacion()
+        {
+            if (Operando1TextBox.Text != "" && Operando2TextBox.Text != "")
+                ResultadoTextBox.Text = (double.Parse(Operando1TextBox.Text) * double.Parse(Operando2TextBox.Text)).ToString();
+        }
+
+        private void Division()
+        {
+            if (Operando1TextBox.Text != "" && Operando2TextBox.Text != "")
+            {
+                if (Operando2TextBox.Text == "0")
+                    ResultadoTextBox.Text = "Error";
+                else
+                    ResultadoTextBox.Text = (double.Parse(Operando1TextBox.Text) / double.Parse(Operando2TextBox.Text)).ToString();
+            }
+        }
+
+        private void RadioButton_Checked_Suma(object sender, RoutedEventArgs e)
+        {
+            Suma();
+        }
+
+        private void RadioButton_Checked_Resta(object sender, RoutedEventArgs e)
+        {
+            Resta();
+        }
+
+        private void RadioButton_Checked_Multipliacion(object sender, RoutedEventArgs e)
+        {
+            Multiplicacion();
+        }
+
+        private void RadioButton_Checked_Division(object sender, RoutedEventArgs e)
+        {
+            Division();            
+        }
+
+        private void OperandoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (SumaCheckBox.IsChecked == true)
+            {
+                Suma();
+            }
+            else if (RestaCheckBox.IsChecked == true)
+            {
+                Resta();
+            }
+            else if (MultiplicacionCheckBox.IsChecked == true)
+            {
+                Multiplicacion();
+            }
+            else if (DivisionCheckBox.IsChecked == true)
+            {
+                Division();
+            }
+
         }
     }
 }
